f0ba7dad1b6e4994676f9c7f01da6623d0f4d3fc
hungpq2 <phamquanghung2008@gmail.com>
2013-09-18 21:25:35 +0700

    no message

---
+++ demoapp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  demoapp
+//
+//  Created by ios17 on 9/18/13.
+//  Copyright (c) 2013 Viettel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *txt;
+
+@end
---
+++ demoapp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  demoapp
+//
+//  Created by ios17 on 9/18/13.
+//  Copyright (c) 2013 Viettel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
